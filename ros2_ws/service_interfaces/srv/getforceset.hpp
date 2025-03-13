839767f7bdfe78b900508d1c9c0f74a97a886d6d
kopiotrek <kopiotrek@gmail.com>
2025-03-13 18:58:00 +0100

    dockerised

---
+++ ros2_ws/service_interfaces/srv/getforceset.hpp
@@ -0,0 +1,11 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef SERVICE_INTERFACES__SRV__GETFORCESET_HPP_
+#define SERVICE_INTERFACES__SRV__GETFORCESET_HPP_
+
+#include "service_interfaces/srv/detail/getforceset__struct.hpp"
+#include "service_interfaces/srv/detail/getforceset__builder.hpp"
+#include "service_interfaces/srv/detail/getforceset__traits.hpp"
+
+#endif  // SERVICE_INTERFACES__SRV__GETFORCESET_HPP_
---
+++ ros2_ws/service_interfaces/srv/gettemp.hpp
@@ -0,0 +1,11 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef SERVICE_INTERFACES__SRV__GETTEMP_HPP_
+#define SERVICE_INTERFACES__SRV__GETTEMP_HPP_
+
+#include "service_interfaces/srv/detail/gettemp__struct.hpp"
+#include "service_interfaces/srv/detail/gettemp__builder.hpp"
+#include "service_interfaces/srv/detail/gettemp__traits.hpp"
+
+#endif  // SERVICE_INTERFACES__SRV__GETTEMP_HPP_
